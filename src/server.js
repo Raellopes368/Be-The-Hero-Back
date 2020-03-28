f8ffe888b73552a296a4880a00cc88b71528c9e3
Raellopes368 <raxbiencute2010@hotmail.com>
2020-03-28 19:35:40 -0300

    Configura servidor basico

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const routes = require('./routes');
+
+class App {
+  constructor() {
+    this.server = express();
+    this.routes();
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
+}
+
+module.exports = new App().server;
